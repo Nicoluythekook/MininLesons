b6c346e0638fdd81a9443c5348ed3c377d9a47cd
Nicoluy Milovidov <milovidov.nicoluy@mail.ru>
2023-07-01 13:51:19 +0300

    Оптимизировал калькулятор

--- app.js
+++ app.js
@@ -22,11 +22,51 @@ const resultElement = document.getElementById('result')
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
 const submitBtn = document.getElementById('submit')
-
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+let action = '+'
 
 //console.log(typeof  sum)
 
+plusBtn.onclick = function () {
+action = '+'
+}
+
+minusBtn.onclick = function () {
+action = '-'
+}
+
+function printResult (result) {
+    if (result < 0) {
+        resultElement.style.color = 'red'
+    } else {
+        resultElement.style.color = 'green'
+    }
+    resultElement.textContent = result
+}
+function computeNumbersWithAction (inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    /* if (actionSymbol === '+') {
+        return num1 + num2
+    }
+
+    if (actionSymbol === '-') {
+        return num1 - num2
+    } */
+    return actionSymbol === '+' ? num1 + num2 : num1 - num2
+}
+
 submitBtn.onclick = function () {
-    const sum = Number(input1.value) + Number(input2.value)
-    resultElement.textContent = sum
- }
+const result = computeNumbersWithAction (inp1, inp2, actionSymbol)
+    printResult(result)
+
+   /* if (action === '+') {
+        const sum = Number(input1.value) + Number(input2.value)
+        printResult(sum)
+
+    } else if (action === '-') {
+        const sum = Number(input1.value) - Number(input2.value)
+        printResult(sum)
+    } */
+}
